440d6005b99ba0118f200f5771ad48c2772ed6ca
vishal marathe <vmarathe36@gmail.com>
2022-06-02 16:50:26 +0530

    new index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+//importing express library to create server
+const express =require ("express") 
+
+const app= express()
+
+app.get("/user",  (req,res) =>{
+      res.send("First Api CALL")
+})
+
+const port =5000;
+app.listen(port,(req,res) =>{
+     console.log(`starting aplication on port no : http ://localhost ${port}`)
+})
